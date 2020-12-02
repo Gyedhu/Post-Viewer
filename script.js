0de4854e07394f08f68048dbc83a5277a01a7002
Yedhu <gyedhumohanan@gmail.com>
2020-12-03 02:48:47 +0530

    added event listener to submit button

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const emailInput = document.querySelector("#email");
 const submit = document.querySelector("#submit");
+
+submit.onclick = () => {
+
+  const email = emailInput.value;
+
+  if (email) {
+    alert(email);
+  } else {
+    alert("Please enter an email");
+  }
+
+}
