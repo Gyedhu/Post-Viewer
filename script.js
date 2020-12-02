ed2618e725932056f1bbcdd43c9ef8cabbcff247
Yedhu <gyedhumohanan@gmail.com>
2020-12-03 03:46:07 +0530

    added styles

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ submit.onclick = async () => {
       const users = await response.json();
 
       let userData = users.filter(user => user.email === email);
-      userData = userData[0]; 
+      userData = userData[0];
+
+      const link = document.createElement("a");
+      link.href = "/profile.html";
+      link.click();
 
     } catch  {
       alert("Error");
